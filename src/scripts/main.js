a23c4b3573a916ce427967a59e9426de519e6c6f
Artur Shmidt <artyrshmidt@gmail.com>
2025-08-08 14:07:44 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const links = [...document.getElementsByClassName('list-item__link')];
+const mainFoto = document.querySelector('.gallery__large-img');
+
+links.forEach((link) => {
+  link.addEventListener('click', (even) => {
+    even.preventDefault();
+    mainFoto.src = link.href;
+  });
+});
